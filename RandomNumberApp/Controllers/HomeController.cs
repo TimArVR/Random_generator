e58b309c9487397589ab6c86bee339fd1f55022e
TimArVR <tsposts@gmail.com>
2025-02-20 13:41:15 +0300

    add site with random number generator

---
+++ RandomNumberApp/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RandomNumberApp.Models;
+
+namespace RandomNumberApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: /Home/
+        public IActionResult Index()
+        {
+            return View(new NumberRangeViewModel());
+        }
+
+        // POST: /Home/GenerateRandomNumbers
+        [HttpPost]
+        public IActionResult GenerateRandomNumbers(NumberRangeViewModel model)
+        {
+            // Проверка валидности модели
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            // Дополнительная проверка: From <= To
+            if (model.From > model.To)
+            {
+                ModelState.AddModelError("", "Начальное значение должно быть меньше или равно конечному.");
+                return View("Index", model);
+            }
+
+            // Генерация списка чисел в диапазоне
+            List<int> numbers = new List<int>();
+            for (int i = model.From; i <= model.To; i++)
+            {
+                numbers.Add(i);
+            }
+
+            // Перемешивание чисел в случайном порядке (алгоритм Фишера-Йетса)
+            Random random = new Random();
+            for (int i = numbers.Count - 1; i > 0; i--)
+            {
+                int j = random.Next(0, i + 1); // Случайный индекс от 0 до i (иногда индекс может меняться сам с собой, это нормальное поведение, чтобы распределение было равновероятным)
+                // Меняем местами элементы
+                int temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
+
+            // Сохраняем перемешанные числа в модель
+            model.ShuffledNumbers = numbers;
+
+            // Передача модели обратно в представление
+            return View("Index", model);
+        }
+    }
+}
---
+++ RandomNumberApp/Models/NumberRangeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RandomNumberApp.Models
+{
+    public class NumberRangeViewModel
+    {
+        [Required(ErrorMessage = "Поле 'От' обязательно для заполнения.")]
+        [Range(-100, 100, ErrorMessage = "Значение 'От' должно быть в диапазоне от -100 до 100.")]
+        public int From { get; set; }
+        [Required(ErrorMessage = "Поле 'До' обязательно для заполнения.")]
+        [Range(-100, 100, ErrorMessage = "Значение 'До' должно быть в диапазоне от -100 до 100.")]
+        public int To { get; set; }
+        public List<int> ShuffledNumbers { get; set; } = new List<int>();
+    }
+}
